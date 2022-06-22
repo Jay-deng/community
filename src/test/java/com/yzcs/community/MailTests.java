6372b5efe81aad4032ef5af842517811730da5c1
yzcs <1471624231@qq.com>
2022-06-23 00:43:22 +0800

    首次提交

---
+++ src/test/java/com/yzcs/community/MailTests.java
@@ -0,0 +1,39 @@
+package com.yzcs.community;
+
+import com.yzcs.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("1471624231@qq.com", "送我的三个字", "你很棒.");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "jay deng");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("1471624231@qq.com", "HTML", content);
+    }
+}
