d2be4437e58ccb2992d99f7016e994364e409c32
yzcs <1471624231@qq.com>
2022-06-23 22:13:30 +0800

    实现注册功能

---
+++ src/main/java/com/yzcs/community/util/CommunityConstant.java
@@ -0,0 +1,10 @@
+package com.yzcs.community.util;
+
+public interface CommunityConstant {
+    // 邮箱激活成功
+    int ACTIVATION_SUCCESS = 0;
+    // 邮箱激活成功
+    int ACTIVATION_REPEAT = 1;
+    // 邮箱激活成功
+    int ACTIVATION_FAILURE= 2;
+}
