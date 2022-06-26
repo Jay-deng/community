ed154da3b94773d588069f77092ac5f48e997905
yzcs <1471624231@qq.com>
2022-06-26 19:36:38 +0800

    随机生成验证码

--- src/main/java/com/yzcs/community/controller/LoginController.java
+++ src/main/java/com/yzcs/community/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.yzcs.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.yzcs.community.entity.User;
 import com.yzcs.community.service.UserService;
 import com.yzcs.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
-public class LoginController  implements CommunityConstant {
+public class LoginController implements CommunityConstant {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -60,5 +74,24 @@ public class LoginController  implements CommunityConstant {
         }
 
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        // 获取验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        // 将验证码存入session
+        session.setAttribute("kaptcha", text);
+
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream(); // 不需要手动关闭，由spring MVC管理
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("响应验证码失败" + e.getMessage());
+        }
     }
 }
