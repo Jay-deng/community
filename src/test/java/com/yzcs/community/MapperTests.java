a94a8332a1efe96f2566ce20d4e0eaa7525f06c3
yzcs <1471624231@qq.com>
2022-07-02 02:06:54 +0800

    私信列表&详情

--- src/test/java/com/yzcs/community/MapperTests.java
+++ src/test/java/com/yzcs/community/MapperTests.java
@@ -2,8 +2,10 @@ package com.yzcs.community;
 
 import com.yzcs.community.Dao.DiscussPostMapper;
 import com.yzcs.community.Dao.LoginTicketMapper;
+import com.yzcs.community.Dao.MessageMapper;
 import com.yzcs.community.entity.DiscussPost;
 import com.yzcs.community.entity.LoginTicket;
+import com.yzcs.community.entity.Message;
 import com.yzcs.community.entity.User;
 import org.junit.Test;
 import com.yzcs.community.Dao.UserMapper;
@@ -29,6 +31,9 @@ public class MapperTests {
 
     @Autowired
     private LoginTicketMapper loginTicketMapper;
+
+    @Autowired
+    private MessageMapper messageMapper;
 
     @Test
     public void testSelectUser() {
@@ -87,7 +92,6 @@ public class MapperTests {
 
     }
 
-
     @Test
     public void testInsertLoginTicket() {
         LoginTicket loginTicket = new LoginTicket();
@@ -107,5 +111,28 @@ public class MapperTests {
         loginTicketMapper.updateStatus("abc", 1);
         loginTicket = loginTicketMapper.selectByTicket("abc");
         System.out.println(loginTicket);
+    }
+
+    @Test
+    public void testSelectLetters() {
+        List<Message> list = messageMapper.selectConversations(111, 0, 20);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112", 0, 10);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+        count = messageMapper.selectLetterUnreadCount(131, "111_131");
+        System.out.println(count);
+
     }
 }
