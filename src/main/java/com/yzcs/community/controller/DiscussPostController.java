26a2351e60d8f1b96665d2b09175aabf4e15c392
yzcs <1471624231@qq.com>
2022-07-01 11:33:28 +0800

    帖子详情

--- src/main/java/com/yzcs/community/controller/DiscussPostController.java
+++ src/main/java/com/yzcs/community/controller/DiscussPostController.java
@@ -3,14 +3,19 @@ package com.yzcs.community.controller;
 import com.yzcs.community.entity.DiscussPost;
 import com.yzcs.community.entity.User;
 import com.yzcs.community.service.DiscussPostService;
+import com.yzcs.community.service.UserService;
 import com.yzcs.community.util.CommunityUtil;
 import com.yzcs.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.awt.*;
+import java.nio.file.Path;
 import java.util.Date;
 
 @Controller
@@ -21,6 +26,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -39,5 +47,19 @@ public class DiscussPostController {
 
         // TODO: 报错的情况， 将来统一处理
         return CommunityUtil.getJSONString(0, "发布成功！");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+
+        // 帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        // 帖子作者
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
+
     }
 }
