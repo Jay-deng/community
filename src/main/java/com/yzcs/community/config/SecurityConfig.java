0311dd57d89f494f18c9ffab71c0ac0f82c3eb7d
yzcs <1471624231@qq.com>
2022-07-14 01:22:36 +0800

    项目监控（actuator，添加自定义端点，配置spring security仅管理员可访问)

--- src/main/java/com/yzcs/community/config/SecurityConfig.java
+++ src/main/java/com/yzcs/community/config/SecurityConfig.java
@@ -52,8 +52,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                 )
                 .antMatchers(
                         "/discuss/delete",
-                        "/data/**"
-//                        "/actuator/**"
+                        "/data/**",
+                        "/actuator/**"
                 )
                 .hasAnyAuthority(
                         AUTHORITY_ADMIN
